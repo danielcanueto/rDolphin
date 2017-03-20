98482b384a256ce89166b258c82139c5bdc52931
user05011988 <dani511988@msn.com>
2017-03-20 13:12:28 +0100

    Update type_plot.R
    Server needs a roxygen form of this function

--- R/type_plot.R
+++ R/type_plot.R
@@ -1,4 +1,16 @@
-
+#' Type of analysis plot
+#'
+#' @param rows_selected Rows selected in GUi table.
+#' @param imported_data List with typical elements necessary to perform quantification of ROIs.
+#' @param ROI_limits Range of plot
+#' @param medianplot Meidan plot 
+#' @param clusterplot Cluster plot 
+#'
+#' @return Plot
+#' @export type_plot
+#' @import plotly
+#' @import reshape2
+#'
 type_plot = function(imported_data,ROI_limits,rows_selected,medianplot,clusterplot) {
 
   range=c(0,max(imported_data$dataset[,which.min(abs(imported_data$ppm-ROI_limits[1])):which.min(abs(imported_data$ppm-ROI_limits[2]))]))
@@ -6,15 +18,14 @@ type_plot = function(imported_data,ROI_limits,rows_selected,medianplot,clusterpl
     print('Wrong selection.')
     return(NULL)
   } else if (rows_selected==1) {
-    clusterplot %>% layout(xaxis = list(range = c(ROI_limits[1], ROI_limits[2]),title='ppm'),yaxis = list(range = range,title='Intensity'))
+    p=clusterplot %>% layout(xaxis = list(range = c(ROI_limits[1], ROI_limits[2]),title='ppm'),yaxis = list(range = range,title='Intensity'))
   }else if (rows_selected==2) {
-    medianplot %>% layout(xaxis = list(range = c(ROI_limits[1], ROI_limits[2]),title='ppm'),yaxis = list(range = range,title='Intensity'))
+    p=medianplot %>% layout(xaxis = list(range = c(ROI_limits[1], ROI_limits[2]),title='ppm'),yaxis = list(range = range,title='Intensity'))
   }else if (all(rows_selected>2)){
     plotdata = data.frame(Xdata=imported_data$ppm, t(imported_data$dataset[rows_selected-2,,drop=F]))
     plotdata <- melt(plotdata, id = "Xdata")
-    plot_ly(data=plotdata,x=~Xdata,y=~ value,color=~variable,type='scatter',mode='lines')%>% layout(xaxis = list(range = c(ROI_limits[1], ROI_limits[2]),title='ppm'),yaxis = list(range = range,title='Intensity'))
-
+    p=plot_ly(data=plotdata,x=~Xdata,y=~ value,color=~variable,type='scatter',mode='lines')%>% layout(xaxis = list(range = c(ROI_limits[1], ROI_limits[2]),title='ppm'),yaxis = list(range = range,title='Intensity'))
   }
 
-
+return(p)
 }
