604066aed4c75a09a30e6792c5b3b82dbdcfa4a1
Daniel Cañueto <danielcanueto88@gmail.com>
2017-08-03 10:14:04 +0200

    Add files via upload

--- R/load_quantification.R
+++ R/load_quantification.R
@@ -28,18 +28,6 @@ Ydata=useful_data[[row]][[col]]$Ydata
 plot_data=useful_data[[row]][[col]]$plot_data
 ROI_profile=useful_data[[row]][[col]]$ROI_profile
 
-#Preparation of figure to be plotted
-# plotdata2 = data.frame(Xdata=Xdata,Ydata=Ydata,plot_data[3, ],plot_data[2, ])
-# colnames(plotdata2)=c('Xdata','Ydata',"fitted_sum","baseline_sum")
-# plotdata2 <- reshape2::melt(plotdata2, id = "Xdata")
-# plotdata2$variable = c(rep('Original Spectrum', length(Ydata)),rep('Generated Spectrum', length(Ydata)),rep('Generated Background', length(Ydata)))
-# plotdata3 = data.frame(Xdata, t(plot_data[-c(1, 2, 3), , drop = F]))
-# colnames(plotdata3)=c('Xdata',rownames(plot_data)[-c(1, 2, 3)])
-# plotdata3 = reshape2::melt(plotdata3, id = "Xdata")
-# r=which(paste(ROI_profile[,4],ROI_profile[,5],sep='_')==imported_data$signals_names[col])
-# plotdata = data.frame(Xdata, signals = plot_data[3 + r, ] )
-# plot_title = paste(imported_data$Experiments[row],"- ROI ",ROI_profile[1,1],"-",ROI_profile[1,2],"ppm")
-# loaded_quantification$plot=suppressWarnings(plot_ly(plotdata3,x = ~Xdata, y = ~value,name="Surrounding signals",type='scatter',mode='lines',fill='tozeroy',fillcolor='rgb(127, 166, 238)') %>% add_trace(data=plotdata2,x=~Xdata,y=~value,color=~variable,type='scatter',mode='lines',name=plotdata2$variable,fill=NULL)  %>% add_trace(data=plotdata,x = ~Xdata, y = ~signals, type = 'scatter', color= imported_data$signals_names[col],mode = 'lines', fill = 'tozeroy',name=plotdata$variable,fillcolor='rgb(60, 60, 60)')   %>%layout(title = plot_title,xaxis = list(range=c(Xdata[1],Xdata[length(Xdata)]),title = 'ppm'), yaxis = list(title = 'Intensity')))
 rownames(plot_data) = c("signals_sum",
                         "baseline_sum",
                         "fitted_sum",
@@ -69,7 +57,10 @@ loaded_quantification$signpar=matrix(NA,2,7)
 colnames(loaded_quantification$signpar)=c("intensity",	"shift",	"half bandwidth",	"gaussian",	"J_coupling",	"multiplicities",	"roof_effect")
 
 if (!is.null(useful_data[[row]][[col]]$signals_parameters)) loaded_quantification$signpar=t(useful_data[[row]][[col]]$signals_parameters)
-	dummy = which(is.na(ROI_data[, 1]))
+if (is.null(colnames(loaded_quantification$signpar))) colnames(loaded_quantification$signpar)=c(as.character(paste(ROI_profile[,4],ROI_profile[,5],sep='_')),rep('additional signal',nrow(loaded_quantification$signpar)-3))
+
+
+dummy = which(is.na(ROI_data[, 1]))
     if (length(dummy)==0) dummy=dim(ROI_data)[1]+1
     lal=which(duplicated(ROI_data[-dummy,1:2])==F)
     ROI_separator = cbind(lal, c(lal[-1] - 1, dim(ROI_data[-dummy,])[1]))
