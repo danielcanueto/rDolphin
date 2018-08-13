1522d088c6c22b75f5cdfe94e8550b192f83dcf3
Daniel Cañueto <danielcanueto88@gmail.com>
2018-08-13 17:38:22 +0200

    Improved version of signparpred
    Improved version of signparpred

--- R/signparpred.R
+++ R/signparpred.R
@@ -18,43 +18,56 @@
 
 
 signparpred=function(initial_matrix,fitting_error=NULL,met_names=NA) {
-  modified_matrix=initial_matrix
-  colnames(modified_matrix)=make.names(colnames(modified_matrix))
-  modified_matrix=jitter(modified_matrix,0.000001*mean(modified_matrix,na.rm=T))
-  dummy=which(apply(modified_matrix,2,function(x) all(is.finite(x)))==T)
+
+  ctrl <- caret::trainControl(method = "boot632",savePredictions="final")
+
+  colnames(initial_matrix)=make.names(colnames(initial_matrix))
+
+  initial_matrix=jitter(initial_matrix,0.000001*mean(initial_matrix,na.rm=T))
+
+  dummy=which(apply(initial_matrix,2,function(x) all(is.finite(x)))==T)
   if (!is.null(fitting_error)) {
   for (i in dummy) {
-    modified_matrix[fitting_error[,i] %in%
+    initial_matrix[fitting_error[,i] %in%
                       boxplot.stats(fitting_error[,i])$out,i]=NA
   }}
-  dummy2=missRanger::missRanger(as.data.frame(modified_matrix[,dummy]))
-  modified_matrix[,dummy]=as.matrix(dummy2)
-  analyzed_signals=which(apply(modified_matrix,2,function(x) all(is.finite(x)))==T)
+  set.seed(1);initial_matrix[,dummy]=as.matrix(missRanger::missRanger(as.data.frame(initial_matrix[,dummy])))
 
-  processed_matrix=data.frame(modified_matrix[,analyzed_signals],
-  prcomp(scale(modified_matrix[,analyzed_signals]))$x[,1:5])
-  processed_matrix=predict(caret::preProcess(processed_matrix,c("center","scale","nzv")),processed_matrix)
-  predicted_matrix=lower_bound_matrix=upper_bound_matrix=as.data.frame(matrix(NA,nrow(modified_matrix),ncol(modified_matrix)))
-  ctrl <- caret::trainControl(method = "boot632",number=18,savePredictions="all")
+  analyzed_signals=which(apply(initial_matrix,2,function(x) all(is.finite(x)))==T)
+
+  predictors=data.frame(initial_matrix[,analyzed_signals],
+  prcomp(scale(initial_matrix[,analyzed_signals]))$x[,1:5])
+  predictors=predict(caret::preProcess(predictors,c("center","scale","nzv")),predictors)
+
+predicted_matrix=lower_bound_matrix=upper_bound_matrix=initial_matrix
+predicted_matrix[,]=lower_bound_matrix[,]=upper_bound_matrix[,]=NA
+
+
   for (i in analyzed_signals) {
     if (is.na(met_names)) {
-      nam=which(colnames(processed_matrix)==colnames(modified_matrix)[i])
-      training_data=data.frame(y=modified_matrix[,i],processed_matrix[,-nam])
-      boruta_output <- Boruta::Boruta(y ~ ., data=training_data, doTrace=0)
+      idx=which(colnames(predictors)==colnames(initial_matrix)[i])
+      training_data=data.frame(y=initial_matrix[,i],predictors[,-idx])
+      set.seed(1);boruta_output <- Boruta::Boruta(y ~ ., data=training_data, doTrace=0)
       ind=colnames(training_data) %in%
         c("y",Boruta::getSelectedAttributes(boruta_output, withTentative = TRUE))
       if (length(which(ind==T))==1) next
-
-      plsFit <- tryCatch(caret::train(y ~ .,data = training_data[,ind],method = "ranger",trControl = ctrl),
+      tgrid=expand.grid(.mtry = 0.5*length(which(ind==T)),
+                        .splitrule="extratrees",
+                        .min.node.size=5)
+      set.seed(1);model <- tryCatch(caret::train(y ~ .,
+                                                  data = training_data[,ind],
+                                                  method = "ranger",
+                                                  tuneGrid=tgrid,
+                                                  trControl = ctrl),
                          error=function(e)
                            caret::train(y ~ .,data = training_data[,ind],method = "rf", trControl = ctrl))
 
     } else {
       sed=intersect(which(met_names==met_names[i]),analyzed_signals)
       if (length(sed)==1) next
-      tel=cbind(modified_matrix[,setdiff(sed,i)],prcomp(scale(modified_matrix[,sed]))$x[,1])
-      training_data=data.frame(y=modified_matrix[,i],scale(tel))
-      boruta_output <- Boruta::Boruta(y ~ ., data=training_data, doTrace=0)
+      tel=cbind(initial_matrix[,setdiff(sed,i)],prcomp(scale(initial_matrix[,sed]))$x[,1])
+      training_data=data.frame(y=initial_matrix[,i],scale(tel))
+      set.seed(1);boruta_output <- Boruta::Boruta(y ~ ., data=training_data, doTrace=0)
       ind=colnames(training_data) %in%
         c("y",Boruta::getSelectedAttributes(boruta_output, withTentative = TRUE))
       if (!is.null(fitting_error)) {
@@ -63,24 +76,34 @@ signparpred=function(initial_matrix,fitting_error=NULL,met_names=NA) {
       } else {
         weights=1/fitting_error[,i]
       }}
-      plsFit <- tryCatch(caret::train(y ~ .,data = training_data[,ind],method = "ranger", weights = weights,trControl = ctrl),
+      tgrid=expand.grid(.mtry = length(which(ind==T))-1,
+                        .splitrule="extratrees",
+                        .min.node.size=5)
+      set.seed(1);model <- tryCatch(caret::train(y ~ .,
+                                                  data = training_data[,ind],
+                                                  method = "ranger",
+                                                  weights = weights,
+                                                  tuneGrid = tgrid,
+                                                  trControl = ctrl),
                          error=function(e)
                          caret::train(y ~ .,data = training_data[,ind],method = "rf", weights = weights,trControl = ctrl))
     }
 
-
-    iii=plsFit$pred
-    ff=sapply(seq(nrow(training_data)),function(x)quantile(rnorm(1000,mean=mean(iii$pred[iii$rowIndex==x],na.rm=T),
-                                                                 sd=sd(iii$pred[iii$rowIndex==x],na.rm=T)),c(0.025,0.5,0.975),na.rm=T))
-    predicted_matrix[,i]=ff[2,]
-    lower_bound_matrix[,i]=ff[1,]
-    upper_bound_matrix[,i]=ff[3,]
+    set.seed(1);predictions=sapply(seq(nrow(training_data)),
+      function(x)
+        quantile(
+          rnorm(1000,mean=mean(model$pred$pred[model$pred$rowIndex==x],na.rm=T),
+                sd=sd(model$pred$pred[model$pred$rowIndex==x],na.rm=T)),
+          c(0.025,0.5,0.975),na.rm=T))
+    predicted_matrix[,i]=predictions[2,]
+    lower_bound_matrix[,i]=predictions[1,]
+    upper_bound_matrix[,i]=predictions[3,]
   }
 
 
-  predicted_matrix=missRanger::missRanger(predicted_matrix)
-  lower_bound_matrix=missRanger::missRanger(lower_bound_matrix)
-  upper_bound_matrix=missRanger::missRanger(upper_bound_matrix)
+  set.seed(1);predicted_matrix=as.matrix(missRanger::missRanger(as.data.frame(predicted_matrix)))
+  set.seed(1);lower_bound_matrix=as.matrix(missRanger::missRanger(as.data.frame(lower_bound_matrix)))
+  set.seed(1);upper_bound_matrix=as.matrix(missRanger::missRanger(as.data.frame(upper_bound_matrix)))
 
   output=list(predicted_matrix=predicted_matrix,lower_bound_matrix=lower_bound_matrix,upper_bound_matrix=upper_bound_matrix)
   return(output)
