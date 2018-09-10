93ef55b410743e09f90a2dcd4d3c66c2dc10fb7d
Daniel Cañueto <danielcanueto88@gmail.com>
2018-09-10 11:33:25 +0200

    Update structure

--- R/signparpred.R
+++ R/signparpred.R
@@ -1,18 +1,19 @@
 
-#' Prediction of signal parameter information with confidence intervals.
+#' Prediction of signal parameter information with prediction intervals.
 #'
 #' @param initial_matrix Matrix of signal paramter values.
 #' @param fitting_error By default NULL. Fitting error associated to the signal parameter values.
 #' @param met_names By default NA. In case of predicting intensity, it enables prediction based  on only signals from the same metabolite. Vector of metabolite associated to each signal whose itensity is predicted.
-#' @return List with predicted values and confidence intervals.
+#' @return List with predicted values and prediction intervals.
 #' @export signparpred
 #' @import caret
 #' @import missRanger
 #' @import Boruta
 #' @examples
 #' # Not run:
-#' # imported_data=import_data(file.path(system.file(package = "rDolphin"),"extdata","Parameters_MTBLS242_15spectra_5groups.csv"))
-#' # load(file.path(system.file(package = "rDolphin"),"extdata","MTBLS242_subset_profiling_data.RData"))
+#' setwd(paste(system.file(package = "rDolphin"),"extdata",sep='/'))
+#' imported_data=import_data("Parameters_MTBLS242_15spectra_5groups.csv")
+#' load("MTBLS242_subset_profiling_data.RData")
 #' # chemical_shift_pred=signparpred(profiling_data$final_output$chemical_shift,profiling_data$final_output$fitting_error)
 #' # intensity_pred=signparpred(profiling_data$final_output$intensity,profiling_data$final_output$fitting_error,imported_data$ROI_data[,4])
 
@@ -35,9 +36,9 @@ signparpred=function(initial_matrix,fitting_error=NULL,met_names=NA) {
 
   analyzed_signals=which(apply(initial_matrix,2,function(x) all(is.finite(x)))==T)
 
-  predictors=data.frame(initial_matrix[,analyzed_signals],
+  features=data.frame(initial_matrix[,analyzed_signals],
   prcomp(scale(initial_matrix[,analyzed_signals]))$x[,1:5])
-  predictors=predict(caret::preProcess(predictors,c("center","scale","nzv")),predictors)
+  features=predict(caret::preProcess(features,c("center","scale","nzv")),features)
 
 predicted_matrix=lower_bound_matrix=upper_bound_matrix=initial_matrix
 predicted_matrix[,]=lower_bound_matrix[,]=upper_bound_matrix[,]=NA
@@ -45,48 +46,48 @@ predicted_matrix[,]=lower_bound_matrix[,]=upper_bound_matrix[,]=NA
 
   for (i in analyzed_signals) {
     if (is.na(met_names)) {
-      idx=which(colnames(predictors)==colnames(initial_matrix)[i])
-      training_data=data.frame(y=initial_matrix[,i],predictors[,-idx])
+      idx=which(colnames(features)==colnames(initial_matrix)[i])
+      training_data=data.frame(y=initial_matrix[,i],features[,-idx])
       set.seed(1);boruta_output <- Boruta::Boruta(y ~ ., data=training_data, doTrace=0)
-      ind=colnames(training_data) %in%
+      selected_features=colnames(training_data) %in%
         c("y",Boruta::getSelectedAttributes(boruta_output, withTentative = TRUE))
-      if (length(which(ind==T))==1) next
-      tgrid=expand.grid(.mtry = 0.5*length(which(ind==T)),
+      if (length(which(selected_features==T))==1) next
+      tgrid=expand.grid(.mtry = 0.5*length(which(selected_features==T)),
                         .splitrule="extratrees",
                         .min.node.size=5)
       set.seed(1);model <- tryCatch(caret::train(y ~ .,
-                                                  data = training_data[,ind],
+                                                  data = training_data[,selected_features],
                                                   method = "ranger",
                                                   tuneGrid=tgrid,
                                                   trControl = ctrl),
                          error=function(e)
-                           caret::train(y ~ .,data = training_data[,ind],method = "rf", trControl = ctrl))
+                           caret::train(y ~ .,data = training_data[,selected_features],method = "rf", trControl = ctrl))
 
     } else {
-      sed=intersect(which(met_names==met_names[i]),analyzed_signals)
-      if (length(sed)==1) next
-      tel=cbind(initial_matrix[,setdiff(sed,i)],prcomp(scale(initial_matrix[,sed]))$x[,1])
-      training_data=data.frame(y=initial_matrix[,i],scale(tel))
+      features=intersect(which(met_names==met_names[i]),analyzed_signals)
+      if (length(features)==1) next
+      predictors=cbind(initial_matrix[,setdiff(features,i)],prcomp(scale(initial_matrix[,features]))$x[,1])
+      training_data=data.frame(y=initial_matrix[,i],scale(predictors))
       set.seed(1);boruta_output <- Boruta::Boruta(y ~ ., data=training_data, doTrace=0)
-      ind=colnames(training_data) %in%
+      selected_features=colnames(training_data) %in%
         c("y",Boruta::getSelectedAttributes(boruta_output, withTentative = TRUE))
-      if (!is.null(fitting_error)) {
-        if (all(is.na(fitting_error[,i]))) {
-        weights=rep(1,nrow(training_data))
-      } else {
-        weights=1/fitting_error[,i]
-      }}
-      tgrid=expand.grid(.mtry = length(which(ind==T))-1,
+      # if (!is.null(fitting_error)) {
+      #   if (all(is.na(fitting_error[,i]))) {
+      #   weights=rep(1,nrow(training_data))
+      # } else {
+      #   weights=1/fitting_error[,i]
+      # }}
+      tgrid=expand.grid(.mtry = length(which(selected_features==T))-1,
                         .splitrule="extratrees",
                         .min.node.size=5)
       set.seed(1);model <- tryCatch(caret::train(y ~ .,
-                                                  data = training_data[,ind],
+                                                  data = training_data[,selected_features],
                                                   method = "ranger",
-                                                  weights = weights,
+                                                  # weights = weights,
                                                   tuneGrid = tgrid,
                                                   trControl = ctrl),
                          error=function(e)
-                         caret::train(y ~ .,data = training_data[,ind],method = "rf", weights = weights,trControl = ctrl))
+                         caret::train(y ~ .,data = training_data[,selected_features],method = "rf", weights = weights,trControl = ctrl))
     }
 
     set.seed(1);predictions=sapply(seq(nrow(training_data)),
